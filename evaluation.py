dbe62c6bf8dc128620796653467f065791042a6e
Milan Rybar <kontakt@milanrybar.cz>
2020-01-13 22:54:40 +0000

    Evaluate all methods in parallel

--- evaluation.py
+++ evaluation.py
@@ -1,24 +1,26 @@
 import os
-from collections import defaultdict
 from functools import partial
 
 import numpy as np
-from joblib import Parallel, delayed, dump
+from joblib import dump
+from jug import TaskGenerator
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import StratifiedKFold
 from sklearn.svm import SVC
 
-from artifacts_removal.ica_removal import remove_artifacts_manual
+from artifacts_removal.ica_removal import (remove_artifacts_manual, remove_artifacts, ic_artifacts_by_kurtosis,
+                                           ic_artifacts_by_peak_values)
 from config import (ANALYSIS_FREQUENCY_START, ANALYSIS_FREQUENCY_END,
                     ANALYSIS_TIME_START, ANALYSIS_TIME_END, RESULTS_DIR)
 from dataset import load_dataset, PATIENTS
+from implementations.csp_matlab import matlab_package_wrapper, matlab_wrapper
 from implementations.csp_python import (csp_wrapper, csp_gep_no_checks, csp_geometric_approach,
                                         csp_geometric_approach_no_checks, csp_gep)
 from implementations.pca_dim_reduction import dim_reduction_pca
-from utils import make_dirs, n_jobs
+from utils import make_dirs
 
 
-def grid_evaluation(dataset, artifact_removal, csp_methods, n_csp_components_list, classifiers, pca_reduction=False):
+def grid_evaluation(dataset, artifact_removal, csp_method, n_csp_components_list, classifiers, pca_reduction=False):
     """
     Evaluation for all options.
     """
@@ -44,27 +46,18 @@ def grid_evaluation(dataset, artifact_removal, csp_methods, n_csp_components_lis
         X_train, X_test = trials[train_index], trials[test_index]
         y_train, y_test = dataset.labels[train_index], dataset.labels[test_index]
 
-        # run CSP for each number of CSP components separately
-        # to have implementation agnostic results
-        # NOTE: run in parallel mainly for Matlab wrapper to speed up opening/closing Matlab for each call
-        split_results = Parallel(n_jobs=n_jobs())(
-            delayed(_classification)(X_train, X_test, y_train, y_test, csp_method, csp_method_name,
-                                     n_csp_components, classifiers, dataset)
-            for csp_method_name, csp_method in csp_methods.items()
+        split_results = {
+            n_csp_components: _classification(X_train, X_test, y_train, y_test,
+                                              csp_method, n_csp_components, classifiers, dataset)
             for n_csp_components in n_csp_components_list
-        )
+        }
+        results.append(split_results)
 
-        # [csp method][#csp components][classifier]
-        split_results_transformed = defaultdict(dict)
-        for r in split_results:
-            split_results_transformed[r['csp_method']][r['n_csp_components']] = r['result']
-        results.append(split_results_transformed)
-
-    # results as [cross-validation split][csp method][#csp components][classifier]
+    # results as [cross-validation split][#csp components][classifier]
     return results
 
 
-def _classification(X_train, X_test, y_train, y_test, csp_method, csp_method_name, n_csp_components, classifiers, dataset):
+def _classification(X_train, X_test, y_train, y_test, csp_method, n_csp_components, classifiers, dataset):
     # compute CSP to get spatial filters
     unmixing_matrix, eigenvalues = csp_method(
         X=X_train, y=y_train, n_csp_components=n_csp_components, dataset=dataset)
@@ -96,11 +89,7 @@ def _classification(X_train, X_test, y_train, y_test, csp_method, csp_method_nam
             'correct_test': correct_test
         }
 
-    return {
-        'csp_method': csp_method_name,
-        'n_csp_components': n_csp_components,
-        'result': result
-    }
+    return result
 
 
 def transform_csp_components_for_classification(data):
@@ -123,65 +112,70 @@ def transform_csp_components_for_classification(data):
     return transformed_data
 
 
-for patient_name in PATIENTS:
-    n_csp_components_list = list(range(2, 22, 2))
+artifact_removal_methods = {
+    'manual':  remove_artifacts_manual,
+    'peak': partial(remove_artifacts, method=ic_artifacts_by_peak_values),
+    'kurtosis': partial(remove_artifacts, method=ic_artifacts_by_kurtosis)
+}
 
-    classifiers = {
-        'lda': LinearDiscriminantAnalysis(),
-        'svm': SVC(kernel='rbf', gamma='auto', C=1.0)  # as default values
-    }
+n_csp_components_list = list(range(2, 22, 2))
 
-    results = grid_evaluation(
-        dataset=load_dataset(patient_name),
-        artifact_removal=remove_artifacts_manual,
-        pca_reduction=False,  # no dimensionality reduction by PCA before CSP
-        csp_methods={
-            # generalized eigenvalue problem approach without any checks
-            # that may have complex solution
-            'gep_no_checks': partial(csp_wrapper, csp_method=csp_gep_no_checks),
+classifiers = {
+    'lda': LinearDiscriminantAnalysis(),
+    'svm': SVC(kernel='rbf', gamma='auto', C=1.0)  # as default values
+}
 
-            # geometric approach (with dimensionality reduction during whitening step)
-            'gap_dr': partial(csp_wrapper, csp_method=partial(csp_geometric_approach)),
+csp_methods = {
+    # generalized eigenvalue problem approach for full rank matrices
+    'pca_gep': partial(csp_wrapper, csp_method=csp_gep),
+    # generalized eigenvalue problem approach without any checks
+    # that may have complex solution
+    'gep_no_checks': partial(csp_wrapper, csp_method=csp_gep_no_checks),
 
-            # geometric approach that may have complex solution for first eigendecomposition
-            # (note that `np.linalg.eig` and `scipy.linalg.eig` have the same behaviour)
-            # i) with dimensionality reduction during whitening step
-            'gap_eig_dr': partial(csp_wrapper, csp_method=partial(csp_geometric_approach_no_checks, eig_method=np.linalg.eig, dim_reduction=True)),
-            # ii) without dimensionality reduction during whitening step
-            'gap_eig': partial(csp_wrapper, csp_method=partial(csp_geometric_approach_no_checks, eig_method=np.linalg.eig, dim_reduction=False)),
-        },
-        n_csp_components_list=n_csp_components_list,
-        classifiers=classifiers
-    )
+    # geometric approach (with dimensionality reduction during whitening step)
+    'gap_dr': partial(csp_wrapper, csp_method=partial(csp_geometric_approach)),
 
-    pca_results = grid_evaluation(
-        dataset=load_dataset(patient_name),
-        artifact_removal=remove_artifacts_manual,
-        pca_reduction=True,  # dimensionality reduction by PCA before CSP
-        csp_methods={
-            # generalized eigenvalue problem approach for full rank matrices
-            'pca_gep': partial(csp_wrapper, csp_method=csp_gep),
-            # generalized eigenvalue problem approach without any checks
-            'pca_gep_no_checks': partial(csp_wrapper, csp_method=csp_gep_no_checks),
+    # geometric approach that may have complex solution for first eigendecomposition
+    # (note that `np.linalg.eig` and `scipy.linalg.eig` have the same behaviour)
+    # i) with dimensionality reduction during whitening step
+    'gap_eig_dr': partial(csp_wrapper, csp_method=partial(csp_geometric_approach_no_checks, eig_method=np.linalg.eig, dim_reduction=True)),
+    # ii) without dimensionality reduction during whitening step
+    'gap_eig': partial(csp_wrapper, csp_method=partial(csp_geometric_approach_no_checks, eig_method=np.linalg.eig, dim_reduction=False)),
 
-            # geometric approach (with dimensionality reduction during whitening step)
-            'pca_gap_dr': partial(csp_wrapper, csp_method=partial(csp_geometric_approach)),
+    # Packages
+    'fieldtrip': partial(matlab_package_wrapper, csp_method='use_fieldtrip'),
+    'bbci': partial(matlab_package_wrapper, csp_method='use_bbci'),
 
-            # geometric approach
-            # (note that `np.linalg.eig` and `scipy.linalg.eig` have the same behaviour)
-            # i) with dimensionality reduction during whitening step
-            'pca_gap_eig_dr': partial(csp_wrapper, csp_method=partial(csp_geometric_approach_no_checks, eig_method=np.linalg.eig, dim_reduction=True)),
-            # ii) without dimensionality reduction during whitening step
-            'pca_gap_eig': partial(csp_wrapper, csp_method=partial(csp_geometric_approach_no_checks, eig_method=np.linalg.eig, dim_reduction=False)),
-        },
-        n_csp_components_list=n_csp_components_list,
-        classifiers=classifiers
-    )
+    # our Matlab implementations
+    'matlab_gep_no_check': partial(matlab_wrapper, csp_method='csp_gep_no_checks'),
+    'matlab_gap_no_check': partial(matlab_wrapper, csp_method='csp_geometric_approach_no_checks')
+}
 
-    joined_results = [{**a, **b} for a, b in zip(results, pca_results)]
 
-    output_path = os.path.join(RESULTS_DIR, 'evaluation')
+@TaskGenerator
+def run_task(patient_name, artifact_removal_name, csp_method_name, pca_reduction, prefix):
+    output_path = os.path.join(RESULTS_DIR, 'evaluation', patient_name)
     make_dirs(output_path)
-    dump(joined_results, os.path.join(output_path, '{}.joblib'.format(patient_name)))
 
-    break
+    try:
+        result = grid_evaluation(
+            dataset=load_dataset(patient_name),
+            artifact_removal=artifact_removal_methods[artifact_removal_name],
+            csp_method=csp_methods[csp_method_name],
+            n_csp_components_list=n_csp_components_list,
+            classifiers=classifiers,
+            pca_reduction=pca_reduction
+        )
+
+        dump(result, os.path.join(output_path, '{}{}_{}.joblib'.format(prefix, artifact_removal_name, csp_method_name)))
+    except Exception as e:
+        # do not save any results on bad CSP implementation
+        dump(e, os.path.join(output_path, '{}{}_{}.exception'.format(prefix, artifact_removal_name, csp_method_name)))
+        print(e)
+
+
+for patient_name in PATIENTS:
+    for artifact_removal_name in artifact_removal_methods.keys():
+        for csp_method_name in csp_methods.keys():
+            run_task(patient_name, artifact_removal_name, csp_method_name, pca_reduction=False, prefix='')
+            run_task(patient_name, artifact_removal_name, csp_method_name, pca_reduction=True, prefix='pca_')
